b01fafa334abfac7add05da498b5651a581eb220
lionel staricoff <staricofflionel@hotmail.com>
2024-05-22 15:16:30 -0500

    actualizando usuario

--- proyecto/backend/src/main/java/com/stockmaster/repository/SalesProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stockmaster.repository;
-
-import com.stockmaster.entity.SalesProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalesProductRepository extends JpaRepository<SalesProduct, Long> {
-}
