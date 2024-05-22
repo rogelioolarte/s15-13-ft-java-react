8c09319ff6e669708869cc4c65694f5f2b581cb7
Rogelio Olarte <galaxino12@gmail.com>
2024-05-22 02:27:56 -0500

    update responsive design of HomePage

--- front/src/components/container/HomeSection.jsx
+++ front/src/components/container/HomeSection.jsx
@@ -7,15 +7,15 @@ export default function HomeSection () {
   return (
     <div className='flex flex-col items-center justify-items-center w-[90%] h-[90%] '>
       <div className='flex flex-col md:flex-row items-center justify-between w-[80%]
-        rounded-[0.5rem] border-[0.1rem] border-black mt-[2%] px-[2%] h-[20%] mb-[3%]'
+        rounded-[0.5rem] border-[0.1rem] border-black mt-[2%] px-[2%] md:h-[20%] mb-[3%]'
       >
         <ActualDate /><SearchBar />
       </div>
       <div className='h-[60%] w-[55%] bg-white border border-[#D6D6D6] rounded-[0.5rem]
         grid grid-flow-row auto-rows-max md:grid-flow-col md:auto-cols-max
-        justify-center md:justify-start content-evenly md:content-around'
+        justify-center md:justify-start content-center items-center justify-items-center md:content-around'
       >
-        <img src={LogoMedium} alt='user' className='bg-black w-[15vh] rounded-[5rem] ml-[20%] ' />
+        <img src={LogoMedium} alt='user' className='bg-black w-[15vh] rounded-[5rem] md:ml-[40%] ' />
         <ShowUserData />
       </div>
       <div className='h-[90%] w-[80%] mt-[5%] border border-black border-solid'>
--- front/src/components/pure/ShowUserData.jsx
+++ front/src/components/pure/ShowUserData.jsx
@@ -1,5 +1,5 @@
 export default function ShowUserData () {
   return (
-    <h1 className='ml-[50%] w-[100%] flex items-center text-[1.2rem]'>Welcome, User</h1>
+    <h1 className='md:ml-[90%] w-[100%] flex items-center text-[1.2rem]'>Welcome, User</h1>
   )
 }
