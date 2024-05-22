6818b5b22008c86dc68a0c23f8f803f7a2e7683f
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2024-05-22 03:40:54 -0300

    feat: add sidebar config: install react icons

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import './index.css'
+import Navbar from './components/container/Navbar'
+import Sidebar from './components/container/Sidebar'
 
 export default function App() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
-    <div>
+    <div className='flex'>
+      <Sidebar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
+      <Navbar openSidebar={openSidebar} setOpenSidebar={setOpenSidebar} />
       <Outlet />
     </div>
   )
