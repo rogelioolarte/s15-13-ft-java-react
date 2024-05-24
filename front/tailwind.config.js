85dbfef48f7155c25251b7655daea46bda529090
S-kkipie <admamaniq@unsa.edu.pe>
2024-05-23 22:45:49 -0500

    ProductPage maquetado

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './index.html',
-    './src/**'
-  ],
+const withMT = require("@material-tailwind/react/utils/withMT");
+export default withMT({
+  content: ["./index.html", "./src/**"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#FFF8F8",
+          20: "#FFF8F820",
+          40: "#FFF8F840",
+          60: "#FFF8F860",
+          80: "#FFF8F880",
+          90: "#FFF8F890",
+        },
+        secondary: {
+          DEFAULT: "#2E90FA",
+          20: "#2E90FA20",
+          40: "#2E90FA40",
+          60: "#2E90FA60",
+          80: "#2E90FA80",
+          90: "#2E90FA90",
+        },
+        warning: {
+          DEFAULT: "#FBB03B",
+          20: "#FBB03B20",
+          40: "#FBB03B40",
+          60: "#FBB03B60",
+          80: "#FBB03B80",
+          90: "#FBB03B90",
+        },
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+});
