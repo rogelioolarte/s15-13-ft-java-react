a49b7d3d570f96b32de847452ff9dc4d3c3b53e8
lionel staricoff <staricofflionel@hotmail.com>
2024-06-10 18:55:20 -0500

    ajustando horario del mapeo para colombia

--- backend/src/main/java/com/stockmaster/service/PurchaseService.java
+++ backend/src/main/java/com/stockmaster/service/PurchaseService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
