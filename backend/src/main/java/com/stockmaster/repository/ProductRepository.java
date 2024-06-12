97a64279da07a986035bceeaf2de123f67aeff8e
lionel staricoff <staricofflionel@hotmail.com>
2024-06-11 21:49:10 -0500

    agregando codigo al ProductRepository

--- backend/src/main/java/com/stockmaster/repository/ProductRepository.java
+++ backend/src/main/java/com/stockmaster/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package com.stockmaster.repository;
 
 import com.stockmaster.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByBarcode(String barcode);
+
+    @Query("SELECT p FROM Product p WHERE p.barcode =:barcode")
+    Product findByBarcode(@Param("barcode") String barcode);
 }
