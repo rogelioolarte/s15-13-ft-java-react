1b23189d8cbe4e68986d9b7b5098cf34690c529f
Rogelio Olarte <rogelioolarte2222@gmail.com>
2024-06-13 15:00:50 -0500

    Update ProductSalesSavedResponse.java

--- backend/src/main/java/com/stockmaster/dto/product/ProductSalesSavedResponse.java
+++ backend/src/main/java/com/stockmaster/dto/product/ProductSalesSavedResponse.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductSalesSavedResponse {
-    private String productName;
+    private String name;
     private String barcode;
     private BigDecimal salePrice;
     @NotNull
